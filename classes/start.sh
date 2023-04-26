a1e65976673165f117690d6201dbd47f75e3207e
lbw <ican2056@163.com>
2023-04-26 21:30:33 +0800

    Update start.sh

--- classes/start.sh
+++ classes/start.sh
@@ -1,7 +1,9 @@
 #！/bin/bash
 
-nohup python3 -m http.server 8881 &
+sed -i 's/Port 8888/Port 8881/g' /etc/tinyproxy/tinyproxy.conf
+service tinyproxy start
 java -cp .:./lib/netty-all-4.1.42.Final.jar ServerService
+
 #nohup java -cp .:./lib/netty-all-4.1.42.Final.jar ServerService >/app/nohup.out 2>&1 &
 #sed -i 's/#Listen 192.168.0.1/Listen 127.0.0.1/g' /etc/tinyproxy/tinyproxy.conf
 #service tinyproxy start
