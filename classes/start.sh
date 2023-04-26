6616ab7b3dc4d6db2025ae69e102c07c790c4deb
lbw <ican2056@163.com>
2023-04-26 19:20:49 +0800

    Update start.sh

--- classes/start.sh
+++ classes/start.sh
@@ -1,5 +1,5 @@
 #！/bin/bash
 
-nohup java -cp .:./lib/netty-all-4.1.42.Final.jar Server service >/app/nohup.out 2>&1 &
+nohup java -cp .:./lib/netty-all-4.1.42.Final.jar ServerService >/app/nohup.out 2>&1 &
 service tinyproxy start
 tail -f /app/nohup.out
