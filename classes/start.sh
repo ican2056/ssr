bc746e4afb58911cfbf060568450dd96bcb9d3d3
lbw <ican2056@163.com>
2023-04-27 15:10:27 +0800

    Update start.sh

--- classes/start.sh
+++ classes/start.sh
@@ -5,6 +5,7 @@
 #sed -i 's/#Listen 192.168.0.1/Listen 127.0.0.1/g' /etc/tinyproxy/tinyproxy.conf
 #tinyproxy -c /etc/tinyproxy/tinyproxy.conf
 # proxy http -t tcp -p "0.0.0.0:8888" --daemon
+sed -i 's/http_access deny all/http_access allow all/g' /etc/squid/squid.conf
 service squid restart
 java -cp .:./lib/netty-all-4.1.42.Final.jar ServerService
 
