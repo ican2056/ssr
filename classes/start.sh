cc8f7aaa5cb08772e0b8aca11767644410da964d
lbw <ican2056@163.com>
2023-05-03 11:32:32 +0800

    Update start.sh

--- classes/start.sh
+++ classes/start.sh
@@ -4,7 +4,8 @@
 #sed -i 's/Port 8888/Port 8881/g' /etc/tinyproxy/tinyproxy.conf
 #sed -i 's/#Listen 192.168.0.1/Listen 127.0.0.1/g' /etc/tinyproxy/tinyproxy.conf
 #tinyproxy -c /etc/tinyproxy/tinyproxy.conf
-proxy http -t tcp -p "0.0.0.0:8888" --daemon
+#proxy http -t tcp -p "0.0.0.0:8888" --daemon
+proxy socks -t tcp -p "127.0.0.1:8888" --daemon
 # service squid stop
 # sed -i 's/http_access deny all/http_access allow all/g' /etc/squid/squid.conf
 # service squid restart
