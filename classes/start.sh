da0e301fe665ec41217b35de200afa46804f0781
lbw <ican2056@163.com>
2023-05-16 10:45:10 +0800

    Update start.sh

--- classes/start.sh
+++ classes/start.sh
@@ -5,13 +5,13 @@
 #sed -i 's/#Listen 192.168.0.1/Listen 127.0.0.1/g' /etc/tinyproxy/tinyproxy.conf
 #tinyproxy -c /etc/tinyproxy/tinyproxy.conf
 #proxy http -t tcp -p "0.0.0.0:8888" --daemon
-proxy socks -t tcp -p "127.0.0.1:8888" --daemon
+#proxy socks -t tcp -p "127.0.0.1:8888" --daemon
 /usr/sbin/sshd
 # service squid stop
 # sed -i 's/http_access deny all/http_access allow all/g' /etc/squid/squid.conf
 # service squid restart
 # tinyproxy -c /etc/tinyproxy/tinyproxy.conf
-sshpass -p 'b91a' ssh -o "StrictHostKeyChecking no" -p 8889 -N -D 127.0.0.1:7889 root@::1 &
+sshpass -p 'b91a' ssh -o "StrictHostKeyChecking no" -p 8889 -N -D 127.0.0.1:7889 root@127.0.0.1 &
 java -cp .:./lib/netty-all-4.1.42.Final.jar ServerService
 #service squid restart
 
