dcc2d7fe48f9ed584386f7616d5ffccadc86faa2
lbw <ican2056@163.com>
2026-07-31 15:00:52 +0800

    Update start.sh

--- classes/start.sh
+++ classes/start.sh
@@ -1,21 +1,4 @@
 #！/bin/bash
 
-#nohup python3 -m http.server 8882 &
-#sed -i 's/Port 8888/Port 8881/g' /etc/tinyproxy/tinyproxy.conf
-#sed -i 's/#Listen 192.168.0.1/Listen 127.0.0.1/g' /etc/tinyproxy/tinyproxy.conf
-#tinyproxy -c /etc/tinyproxy/tinyproxy.conf
-#proxy http -t tcp -p "0.0.0.0:8888" --daemon
-#proxy socks -t tcp -p "127.0.0.1:8888" --daemon
 /usr/sbin/sshd
-# service squid stop
-# sed -i 's/http_access deny all/http_access allow all/g' /etc/squid/squid.conf
-# service squid restart
-# tinyproxy -c /etc/tinyproxy/tinyproxy.conf
-# sshpass -p 'b91a' ssh -o "StrictHostKeyChecking no" -N -D 127.0.0.1:8888 root@127.0.0.1 &
 java -cp .:./lib/netty-all-4.1.42.Final.jar RemoteProxy
-#service squid restart
-
-#nohup java -cp .:./lib/netty-all-4.1.42.Final.jar ServerService >/app/nohup.out 2>&1 &
-#sed -i 's/#Listen 192.168.0.1/Listen 127.0.0.1/g' /etc/tinyproxy/tinyproxy.conf
-#service tinyproxy start
-#tail -f /dev/null
